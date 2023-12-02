c2246cd998bce0309676ffb8e872aff83637b1f6
VascoII <fabriceds@yahoo.fr>
2023-12-02 18:03:56 +0100

    Create Voting smart contract skeleton

---
+++ 2. Solidity/Projet1/Voting.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+    
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    event VoterRegistered(address voterAddress); 
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
+
+    constructor() Ownable(msg.sender) {}
+
+}
